d2db797af5bf17519e1e37d6f55e5dbb405fe470
LAPTOP-H92P4FEU\user <sakhilemthembu360@gmail.com>
2026-04-24 13:32:45 +0200

    Initial commit of the solution

---
+++ SeatSync/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeatSync.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
